93c9a87baeb3bba6f24f5a7021d5f496e616f491
ab.reza <a.re.morteza@gmail.com>
2021-07-13 10:20:48 +0430

    init buy and deligation for bank

--- nodes/bank/main.py
+++ nodes/bank/main.py
@@ -37,9 +37,60 @@ def createAccount(request):
         return 'Error: user already exist!'
 
 
+def deliagte(request):
+    try:
+        username = request['username']
+        password = request['password']
+        public_key = request['public_key']
+        private_key = request['private_key']
+        policy_range = request['policy_range']
+        policy_count = request['policy_count']
+        policy_timeout = request['policy_timeout']
+    except:
+        return 'Error: bad request!'
+
+    try:
+        user = User.authenticate(username, password)
+    except User.DoesNotExist:
+        return 'Error: invalid username or password!'
+
+    user.deligate(public_key,
+                  private_key,
+                  policy_range,
+                  policy_count,
+                  policy_timeout)
+    return 'OK'
+
+
+def buyWithCoin(request):
+    username = request.get('username')
+    password = request.get('password')
+    receiver = request.get('receiver')
+    amount = request.get('amount')
+
+    if not username or not password or not receiver or not amount:
+        return 'Error: bad request! Req should have username, password, receiver and amount fields.'
+    try:
+        user = User.authenticate(username, password)
+    except User.DoesNotExist:
+        return 'Error: invalid username or password!'
+
+    try:
+        receiver = User.select().where(username == receiver)
+    except User.DoesNotExist:
+        return 'Error: receiver does not exist!'
+
+    try:
+        user.exchangeCoin(receiver, amount)
+    except Exception as e:
+        return 'Error: ' + e
+
+
 router = {
     'checkBalance': checkBalance,
-    'createAccount': createAccount
+    'createAccount': createAccount,
+    'deliagte': deliagte,
+    'buyWithCoin': buyWithCoin,
 }
 
 import configparser
--- nodes/bank/models.py
+++ nodes/bank/models.py
@@ -1,7 +1,14 @@
+import json
+from connection.tcp_socket import send_message
 from utils import check_password, make_password
 from peewee import *
+from datetime import datetime
 
 from os.path import exists
+
+import configparser
+config = configparser.ConfigParser()
+config.read('../../app.cfg')
 
 db_name = 'db.sqlite3'
 database = SqliteDatabase(db_name)
@@ -18,14 +25,30 @@ class BlockchainDeligation(BaseModel):
     policy_range = IntegerField()
     policy_count = IntegerField()
     policy_timeout = DateTimeField()
-    policy_receiver = CharField()
+    policy_receiver = CharField(null=True)
+
+    def check_policy(self, amount, receiver):
+        if amount > self.policy_range:
+            raise Exception('deligation policy range issue!')
+
+        if datetime.now() > self.policy_timeout:
+            raise Exception('deligation expired!')
+
+    def sellCoin(self, amount):
+        ip = config['EC']['IP']
+        port = int(config['EC']['Port'])
+        message = json.dump(
+            {"url": "sell", "public_key": self.public_key, "amount": amount})
+        response = send_message(ip, port, message)
+        if response != 'OK':
+            raise Exception('blockchain error ' + response)
 
 
 class User(BaseModel):
     username = CharField(unique=True, max_length=128)
     password = CharField(max_length=128, default='pass')
     balance = IntegerField(default=1000000)
-    blockchainAccount = ForeignKeyField(
+    blockchainDeligation = ForeignKeyField(
         BlockchainDeligation, null=True, unique=True)
 
     def set_password(self, raw_password):
@@ -33,6 +56,28 @@ class User(BaseModel):
 
     def check_password(self, raw_password):
         return check_password(raw_password, self.password)
+
+    def sellCoin(self, amount):
+        self.blockchainDeligation.sellCoin(amount)
+        self.balance += amount
+        self.save()
+
+    def exchangeCoin(self, receiver, amount):
+        if not self.blockchainDeligation:
+            raise Exception('deligation was not set!')
+
+        self.blockchainDeligation.check_policy(amount, receiver)
+        self.sellCoin(amount)
+
+        transaction = Transaction.create(
+            sender=self, receiver=receiver, amount=amount)
+        transaction.save()
+
+    def deligate(self, public_key, private_key, policy_range, policy_count, policy_timeout):
+        self.blockchainDeligation = BlockchainDeligation.create(public_key, private_key,
+                                                                policy_range, policy_count, policy_timeout)
+        self.blockchainDeligation.save()
+        self.save()
 
     @staticmethod
     def authenticate(username, password):
